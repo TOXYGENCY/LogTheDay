919e3747224b6f90d6e848e61e4069a92662394d
TOXYGENCY <mailforsomethingelse@gmail.com>
2024-11-04 15:27:53 +0300

    Изменение структуры файлов проекта.

--- obj/Debug/net8.0/LogTheDay.AssemblyInfo.cs
+++ obj/Debug/net8.0/LogTheDay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogTheDay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e10d1763d0f2bc9e8bccd5238eb7c304e86aecb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bbb1be68f664560f0a8b83a58f27d1353ef5aa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogTheDay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogTheDay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
